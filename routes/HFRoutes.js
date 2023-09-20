a71f34ad25eeba5c94f4a70520fcd04b6a1221f5
Manish03Singh <manish031627@gmail.com>
2023-09-20 23:33:07 +0530

    added packages

--- routes/HFRoutes.js
+++ routes/HFRoutes.js
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import * as dotenv from 'dotenv';
 import { HfInference } from "@huggingface/inference";
+import fetch from "node-fetch";
 
 dotenv.config();
 const router = express.Router();
